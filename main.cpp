45b15d8f507721a3c0c3793ad4cb2af6d50ee568
RedShy <dcdavide40@gmail.com>
2020-06-29 20:29:45 +0200

    added GA

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "1+1-ES.h"
 #include "mu+lambda-ES.h"
 #include "mu_comma_lambda-ES.h"
+#include "GA.h"
 using namespace std;
 
 int main(int argc, char **argv)
@@ -17,8 +18,9 @@ int main(int argc, char **argv)
     int depots = 0;
     int customers = 0;
     double **coordinate_matrix;
+    //string file = "./dat/10x4-1.txt";
     string file = "./dat/pr10.txt";
-    int vehicles = 5;
+    int vehicles = 35;
 
     //read_file_cordeau(file, depots, vehicles, customers, coordinate_matrix);
     read_file_ruiz(file, depots, customers, coordinate_matrix);
@@ -35,12 +37,12 @@ int main(int argc, char **argv)
     switch (heuristic)
     {
     case 0:
-        best = ES_1_1(depots, customers, vehicles, distance_matrix, 80000);
+        best = ES_1_1(depots, customers, vehicles, distance_matrix, 8000000);
         cout << "BEST: " << best << "\n";
         for (int i = 1; i < 1600000; i++)
         {
             //cout << "TENTATIVO: " << i << endl;
-            double result = ES_1_1(depots, customers, vehicles, distance_matrix, 80000);
+            double result = ES_1_1(depots, customers, vehicles, distance_matrix, 8000000);
             if (result < best)
             {
                 best = result;
@@ -49,41 +51,36 @@ int main(int argc, char **argv)
         }
         break;
     case 1:
-        best = ES_mu_plus_lambda(depots, customers, vehicles, distance_matrix, 5000, 15, 100);
-        cout << "BEST: " << best << "\n";
-        for (int i = 1; i < 16000; i++)
-        {
-            //cout << "TENTATIVO: " << i << endl;
-            double result = ES_mu_plus_lambda(depots, customers, vehicles, distance_matrix, 5000, 15, 100);
-            if (result < best)
-            {
-                best = result;
-                cout << "BEST: " << best << "\n";
-            }
-        }
+        best = ES_mu_plus_lambda(depots, customers, vehicles, distance_matrix, 50000, 15, 250);
         break;
     case 2:
-        best = ES_mu_comma_lambda(depots, customers, vehicles, distance_matrix, 5000, 15, 200);
-        cout << "BEST: " << best << "\n";
-        for (int i = 1; i < 16000; i++)
-        {
-            //cout << "TENTATIVO: " << i << endl;
-            double result = ES_mu_comma_lambda(depots, customers, vehicles, distance_matrix, 5000, 15, 200);
-            if (result < best)
-            {
-                best = result;
-                cout << "BEST: " << best << "\n";
-            }
-        }
+        best = ES_mu_comma_lambda(depots, customers, vehicles, distance_matrix, 500000, 15, 300);
         break;
     case 3:
-        //TODO GA
+        GA(depots, customers, vehicles, distance_matrix, 500000, 100, 4);
         break;
+    case 4:
+        //benchmark test
+        best = ES_mu_plus_lambda_test(depots, customers, vehicles, distance_matrix, 3000, 15, 300);
+        break;
+    case 5:
+        best = ES_mu_plus_lambda_adaptive(depots, customers, vehicles, distance_matrix, 50000, 15, 250);
     }
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
     cout << "Finale: " << best << endl;
+
+    Individual a(vehicles, depots, customers, distance_matrix);
+    // Individual b(vehicles, depots, customers, distance_matrix);
+    // Individual c(vehicles, depots, customers, distance_matrix);
+     a.random_inizialize();
+    // b.random_inizialize();
+    // c.partially_mapped_cross_over(a, b);
+
+    a.print_tour_matrix();
+    a.insertion_repeated();
+    a.print_tour_matrix();
 
     return 0;
 }
